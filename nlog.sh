d27f735bfa8d920eaf4edbd1227a099f580f2534
Jose Ignacio Seco <jose.ignacio.seco@ericsson.com>
2021-12-27 13:17:19 +0100

    We need to reverse again
    Change-Id: Ie00dba7aba31c700c3773f12ef695c2db8683f84

--- nlog.sh
+++ nlog.sh
@@ -2,7 +2,7 @@
 
 log_dir=${DEVDIR}/logs
 
-log_dir_name=$(ls -ltr ${log_dir} | tail -1 | rev | cut -d ' ' -f 1)
+log_dir_name=$(ls -ltr ${log_dir} | tail -1 | rev | cut -d ' ' -f 1 | rev)
 
 log_dir_path=${log_dir}/${log_dir_name}
 
