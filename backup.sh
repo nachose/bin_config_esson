9d860f21255d287b1618e32007150937180d3cab
Jose Ignacio Seco <jose.ignacio.seco@ericsson.com>
2022-01-03 10:56:43 +0100

    More changes to configuration
    Change-Id: I644bbd4d3879f291c4e211657d6eb794bda894c2

--- backup.sh
+++ backup.sh
@@ -4,14 +4,12 @@ route=/home/esecjos/backup
 filename=${route}/backup_$(date +%j).tar
 
 if [[ -f ${filename} ]]; then
-	echo "File exists"
 	exit 0
 fi
 
 tar -C / -cvf ${filename} home/esecjos/.cshrc.user > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.bashrc.user > /dev/null
-tar -C / -rvf ${filename} home/esecjos/bin/backup.sh > /dev/null
-tar -C / -rvf ${filename} home/esecjos/bin/nlog.sh > /dev/null
+tar -C / -rvf ${filename} home/esecjos/bin/* > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.gitconfig > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.vim > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.vimrc > /dev/null
---
+++ nwarnlog.bash
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+log_dir=${DEVDIR}/logs
+
+log_dir_name=$(ls -ltr ${log_dir} | tail -1 | rev | cut -d ' ' -f 1 | rev)
+
+log_dir_path=${log_dir}/${log_dir_name}
+
+grep -rIi -B1 -A1 '\<warning\>' ${log_dir_path}
