24c03b9a0b221a37271e7515969de6bcd289572b
Jose Ignacio Seco <jose.ignacio.seco@ericsson.com>
2022-01-13 09:08:45 +0100

    bin directory was not being backed up
    Change-Id: Ibf8d1dd11f361a11fe78926389e23e7d7bdc19d2

--- backup.sh
+++ backup.sh
@@ -9,12 +9,12 @@ fi
 
 tar -C / -cvf ${filename} home/esecjos/.cshrc.user > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.bashrc.user > /dev/null
-tar -C / -rvf ${filename} home/esecjos/bin/* > /dev/null
+tar -C / -rvf ${filename} home/esecjos/bin > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.gitconfig > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.gdbinit > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.vim > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.vimrc > /dev/null
 
 
-find ${route} -mtime +30 -delete
+find ${route} -mtime +15 -delete
 
