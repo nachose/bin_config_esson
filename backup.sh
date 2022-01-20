cb028c6b1f48f1d23bd151e429bdafb0e66378e6
Jose Ignacio Seco <jose.ignacio.seco@ericsson.com>
2022-01-20 15:35:43 +0100

    More changes
    Change-Id: Ifbf1edf30552f1ad5691ab0690e5e77b010c2a01

--- backup.sh
+++ backup.sh
@@ -9,11 +9,12 @@ fi
 
 tar -C / -cvf ${filename} home/esecjos/.cshrc.user > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.bashrc.user > /dev/null
-tar -C / -rvf ${filename} home/esecjos/bin > /dev/null
+tar -C / -rvf ${filename} --exclude=.git home/esecjos/bin > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.gitconfig > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.gdbinit > /dev/null
-tar -C / -rvf ${filename} home/esecjos/.vim > /dev/null
+tar -C / -rvf ${filename} --exclude=plugged home/esecjos/.vim > /dev/null
 tar -C / -rvf ${filename} home/esecjos/.vimrc > /dev/null
+#tar -C / -rvf ${filename} $(find -P /home/esecjos -type l) > /dev/null
 
 
 find ${route} -mtime +15 -delete
