6e176bc268c7f8e59b83fd010cfbef1fe7719cb0
Jose Ignacio Seco <jose.ignacio.seco@ericsson.com>
2022-02-28 09:21:46 +0100

    more config
    Change-Id: I95c4a6de644cd72d1cc41c1962a42c9dc81282e2

--- copy_files.bash
+++ copy_files.bash
@@ -1,11 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 BIN_DIR=/home/esecjos/bin
 
-cp ~/.cshrc.user $BIN_DIR 
+cp ~/.cshrc.user $BIN_DIR
 cp ~/.bashrc.user $BIN_DIR
-cp ~/.gdbinit $BIN_DIR 
+cp ~/.gdbinit $BIN_DIR
 cp ~/.gitconfig $BIN_DIR
 cp ~/.vimrc $BIN_DIR
+cp ~/.screenrc $BIN_DIR
 
 
