e48f67b14203fd67050819b52e282e2efc8c67a7
Jose Ignacio Seco <jose.ignacio.seco@ericsson.com>
2022-03-07 11:11:02 +0100

    More changes
    Change-Id: I77eeef31b02a61c2880987903941417c45fca0ef

--- backup_modif_files.bash
+++ backup_modif_files.bash
@@ -4,10 +4,10 @@ file_name=backup_modif_files_$1_$(date +%j_%s).tar
 
 git status | grep -e 'new file\|modified file' | cut -f 2 -d : | xargs tar -rvf ${file_name}
 
-mv $[file_name] ~/code_backup
+mv ${file_name} ~/code_backup
 
 cd ~/code_backup/
 
 
 #Delete files older than three days.
-find ${route} -mtime +3 -delete
+find ~/code_backup/ -mtime +3 -delete
